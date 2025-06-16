651b41ac2719aecdaea892675e60ec870de5b795
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-16 20:12:59 +0545

    FInal

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// https://vite.dev/config/
+// Define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
- 
-})
+    plugins: [react(), tailwindcss(),],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'myLib',
+            fileName: 'myLib',
+        },
+        rollupOptions: {
+            external: [/^node:\w+/], // <-- ignores all 'node:*'
+        },
+    },
+});
